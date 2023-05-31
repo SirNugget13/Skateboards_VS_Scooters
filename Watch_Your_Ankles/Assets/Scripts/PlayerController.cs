1a3967f7dd9c4247be5dae391fa17f09501f5111
Person1246811 <vvvvRussellvvvv@gmail.com>
2023-05-31 10:18:42 -0700

    Movement

--- Watch_Your_Ankles/Assets/Scripts/PlayerController.cs
+++ Watch_Your_Ankles/Assets/Scripts/PlayerController.cs
@@ -5,22 +5,35 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public bool player1;
+    private Rigidbody2D myRB;
+
+    public bool player2;
 
     public float speed;
+    private Vector2 move;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (player1)
+        if (!player2)
         {
-
+            if (Gamepad.current == null)
+                move.x = Input.GetAxis("Horizontal");
+            else
+                move.x = Gamepad.all[0].leftStick.value.x;
         }
+        else
+        {
+            move.x = Gamepad.all[1].leftStick.value.x;
+        }
+        Vector2 temp = move * speed;
+        if (Input.GetMouseButtonDown(0))
+            myRB.AddForce(new Vector2(temp.x, 0));
     }
 }
